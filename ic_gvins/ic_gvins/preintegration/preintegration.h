9b0a7e849eec3fa299a5a615a6f89db6228fb3b2
WZXTP <145897967+WZXTP@users.noreply.github.com>
2024-07-03 20:47:41 +0800

    Update preintegration.h

--- ic_gvins/ic_gvins/preintegration/preintegration.h
+++ ic_gvins/ic_gvins/preintegration/preintegration.h
@@ -20,6 +20,13 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/*
+定义了 Preintegration 类，该类用来选择并创建适当的预积分处理器。
+预积分（Preintegration）用于在惯性测量单元（IMU）中处理加速度计和陀螺仪数据，
+从而在位姿优化中有效地结合IMU数据。此文件中的核心功能是根据不同的配置选项动态选择和创建合适的预积分对象，
+以及管理这些对象的数据转换和参数处理。
+*/
+
 #ifndef PREINTEGRATION_H
 #define PREINTEGRATION_H
 
@@ -29,20 +36,21 @@
 #include "preintegration/preintegration_normal.h"
 #include "preintegration/preintegration_odo.h"
 
-class Preintegration {
+class Preintegration {//Preintegration 类定义了一个公共接口，用于创建和管理不同类型的预积分对象。
 
 public:
     Preintegration() = default;
 
-    enum PreintegrationOptions {
-        PREINTEGRATION_NORMAL    = 0,
-        PREINTEGRATION_ODO       = 1,
-        PREINTEGRATION_EARTH     = 2,
-        PREINTEGRATION_EARTH_ODO = 3,
+    enum PreintegrationOptions {//PreintegrationOptions 枚举类型定义了四种不同的预积分选项
+        PREINTEGRATION_NORMAL    = 0,//正常预积分
+        PREINTEGRATION_ODO       = 1,//带有里程计的预积分
+        PREINTEGRATION_EARTH     = 2,//考虑地球模型的预积分
+        PREINTEGRATION_EARTH_ODO = 3,//考虑地球模型并带有里程计的预积分
     };
 
     static PreintegrationOptions getOptions(const IntegrationConfiguration &config) {
         int options = PREINTEGRATION_NORMAL;
+        //通过累加相应的枚举值来确定最终的预积分类型
 
         if (config.isuseodo) {
             options += PREINTEGRATION_ODO;
@@ -52,14 +60,17 @@ public:
         }
 
         return static_cast<PreintegrationOptions>(options);
+        //使用 static_cast 进行类型转换，确保返回的是 PreintegrationOptions 类型。
     }
 
-    static std::shared_ptr<PreintegrationBase>
+//创建预积分对象    
+static std::shared_ptr<PreintegrationBase>
         createPreintegration(const std::shared_ptr<IntegrationParameters> &parameters, const IMU &imu0,
                              const IntegrationState &state, PreintegrationOptions options) {
-        std::shared_ptr<PreintegrationBase> preintegration;
+        //根据提供的参数和预积分选项创建一个具体的预积分对象
+        std::shared_ptr<PreintegrationBase> preintegration;//确保返回的对象可以共享管理其生命周期。
 
-        if (options == PREINTEGRATION_NORMAL) {
+        if (options == PREINTEGRATION_NORMAL) {//通过条件语句来选择合适的预积分类的实例化
             preintegration = std::make_shared<PreintegrationNormal>(parameters, imu0, state);
         } else if (options == PREINTEGRATION_ODO) {
             preintegration = std::make_shared<PreintegrationOdo>(parameters, imu0, state);
@@ -72,10 +83,11 @@ public:
         return preintegration;
     }
 
-    static int numPoseParameter() {
-        return PreintegrationBase::NUM_POSE;
+    static int numPoseParameter() {//获取姿态参数的数量
+        return PreintegrationBase::NUM_POSE;//该值是通过调用 PreintegrationBase 类的静态成员 NUM_POSE 来获取的
     }
 
+    //状态与数据的转换
     static IntegrationStateData stateToData(const IntegrationState &state, PreintegrationOptions options) {
         if (options == PREINTEGRATION_NORMAL) {
             return PreintegrationNormal::stateToData(state);
@@ -89,6 +101,7 @@ public:
         return {};
     }
 
+    //获取混合参数的数量
     static IntegrationState stateFromData(const IntegrationStateData &data, PreintegrationOptions options) {
         if (options == PREINTEGRATION_NORMAL) {
             return PreintegrationNormal::stateFromData(data);
