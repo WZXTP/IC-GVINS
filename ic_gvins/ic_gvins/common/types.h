075a3a63bebbb120915adc57aeab47143f282709
WZXTP <145897967+WZXTP@users.noreply.github.com>
2024-07-10 15:43:50 +0800

    Update types.h

--- ic_gvins/ic_gvins/common/types.h
+++ ic_gvins/ic_gvins/common/types.h
@@ -20,6 +20,11 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/*
+这段代码定义了几个结构体和使用了 Eigen 库的类型别名，
+用于描述不同类型的数据结构，如GNSS数据、PVA数据、IMU数据和姿态数据。
+*/
+
 #ifndef TYPES_H
 #define TYPES_H
 
@@ -30,36 +35,36 @@ using Eigen::Quaterniond;
 using Eigen::Vector3d;
 
 typedef struct GNSS {
-    double time;
+    double time;// 时间戳
 
-    Vector3d blh;
-    Vector3d std;
+    Vector3d blh;// 包含纬度、经度、高度的向量
+    Vector3d std;// 标准差向量，用于描述位置的测量精度
 
-    bool isyawvalid;
-    double yaw;
+    bool isyawvalid;// 指示航向角是否有效的布尔值
+    double yaw;// 航向角度，表示方向
 } GNSS;
 
 typedef struct PVA {
-    double time;
+    double time;// 时间戳
 
-    Vector3d blh;
-    Vector3d vel;
-    Vector3d att;
+    Vector3d blh;// 包含纬度、经度和高度的向量
+    Vector3d vel;// 速度向量
+    Vector3d att;// 姿态向量，可能表示方向或者角度
 } PVA;
 
 typedef struct IMU {
     double time;
-    double dt;
+    double dt;// 时间间隔
 
-    Vector3d dtheta;
-    Vector3d dvel;
+    Vector3d dtheta;// 角度增量向量
+    Vector3d dvel;// 速度增量向量
 
-    double odovel;
+    double odovel;// 其他速度参数
 } IMU;
 
 typedef struct Pose {
-    Matrix3d R;
-    Vector3d t;
+    Matrix3d R;// 3x3 的旋转矩阵，描述物体的姿态
+    Vector3d t;// 位置向量，描述物体的位置
 } Pose;
 
 #endif // TYPES_H
