af09c6d79f2b1bf2d7c5cbc5826376bff67bc72b
WZXTP <145897967+WZXTP@users.noreply.github.com>
2024-07-15 10:02:25 +0800

    Update preintegration_normal.cc

--- ic_gvins/ic_gvins/preintegration/preintegration_normal.cc
+++ ic_gvins/ic_gvins/preintegration/preintegration_normal.cc
@@ -20,6 +20,12 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/*
+PreintegrationNormal类通过继承PreintegrationBase类，扩展了预积分的功能。
+它包括初始化、评估、状态构造、状态重置以及更新雅克比矩阵和协方差矩阵的功能。
+这些功能使得该类可以处理复杂的IMU数据，并进行准确的状态估计。
+*/
+
 #include "preintegration/preintegration_normal.h"
 
 #include "common/logging.h"
@@ -28,20 +34,24 @@ PreintegrationNormal::PreintegrationNormal(std::shared_ptr<IntegrationParameters
                                            IntegrationState state)
     : PreintegrationBase(std::move(parameters), imu0, std::move(state)) {
 
-    // Reset state
+    // Reset state，重置状态
     resetState(state, NUM_STATE);
 
-    // Set initial noise matrix
+    // Set initial noise matrix，设置噪声矩阵
     setNoiseMatrix();
 }
 
+//用于计算两个积分状态之间的残差，并将其返回。该函数对状态进行了积分校正，并计算了残差的平方根信息矩阵。
 Eigen::MatrixXd PreintegrationNormal::evaluate(const IntegrationState &state0, const IntegrationState &state1,
                                                double *residuals) {
+    // 计算残差的平方根信息矩阵
     sqrt_information_ =
         Eigen::LLT<Eigen::Matrix<double, NUM_STATE, NUM_STATE>>(covariance_.inverse()).matrixL().transpose();
 
+    // 将残差映射到 Eigen 矩阵
     Eigen::Map<Eigen::Matrix<double, NUM_STATE, 1>> residual(residuals);
 
+    // 提取雅克比矩阵中的部分块
     Matrix3d dp_dbg = jacobian_.block<3, 3>(0, 9);
     Matrix3d dp_dba = jacobian_.block<3, 3>(0, 12);
     Matrix3d dv_dbg = jacobian_.block<3, 3>(3, 9);
@@ -57,7 +67,7 @@ Eigen::MatrixXd PreintegrationNormal::evaluate(const IntegrationState &state0, c
     corrected_v_ = delta_state_.v + dv_dba * dba + dv_dbg * dbg;
     corrected_q_ = delta_state_.q * Rotation::rotvec2quaternion(dq_dbg * dbg);
 
-    // Residuals
+    // Residuals，计算残差
     residual.block<3, 1>(0, 0) = state0.q.inverse() * (state1.p - state0.p - state0.v * delta_time_ -
                                                        0.5 * gravity_ * delta_time_ * delta_time_) -
                                  corrected_p_;
@@ -66,38 +76,49 @@ Eigen::MatrixXd PreintegrationNormal::evaluate(const IntegrationState &state0, c
     residual.block<3, 1>(9, 0)  = state1.bg - state0.bg;
     residual.block<3, 1>(12, 0) = state1.ba - state0.ba;
 
+    // 用平方根信息矩阵缩放残差
     residual = sqrt_information_ * residual;
     return residual;
 }
 
 Eigen::MatrixXd PreintegrationNormal::residualJacobianPose0(const IntegrationState &state0,
                                                             const IntegrationState &state1, double *jacobian) {
+    // 将 jacobian 指针映射到一个 Eigen 矩阵
     Eigen::Map<Eigen::Matrix<double, NUM_STATE, NUM_POSE, Eigen::RowMajor>> jaco(jacobian);
-    jaco.setZero();
+    jaco.setZero(); // 初始化为零矩阵
 
+    // 填充雅克比矩阵的块
+    //计算残差对初始位姿 state0 位置部分的雅克比矩阵，取负的旋转矩阵。
     jaco.block(0, 0, 3, 3) = -state0.q.inverse().toRotationMatrix();
+    //计算残差对初始位姿 state0 方向部分的雅克比矩阵，使用旋转矩阵和重力等信息。
     jaco.block(0, 3, 3, 3) =
         Rotation::skewSymmetric(state0.q.inverse() * (state1.p - state0.p - state0.v * delta_time_ -
                                                       0.5 * gravity_ * delta_time_ * delta_time_));
+    //计算残差对初始位姿 state0 速度部分的雅克比矩阵，使用旋转矩阵和重力等信息。
     jaco.block(3, 3, 3, 3) =
         Rotation::skewSymmetric(state0.q.inverse() * (state1.v - state0.v - gravity_ * delta_time_));
+    //计算残差对初始位姿 state0 方向部分的雅克比矩阵，使用四元数乘法和旋转矩阵。
     jaco.block(6, 3, 3, 3) =
         -(Rotation::quaternionleft(state1.q.inverse() * state0.q) * Rotation::quaternionright(corrected_q_))
              .bottomRightCorner<3, 3>();
 
+    // 用平方根信息矩阵缩放雅克比矩阵
     jaco = sqrt_information_ * jaco;
     return jaco;
 }
 
 Eigen::MatrixXd PreintegrationNormal::residualJacobianPose1(const IntegrationState &state0,
                                                             const IntegrationState &state1, double *jacobian) {
+    // 将 jacobian 指针映射到一个 Eigen 矩阵
     Eigen::Map<Eigen::Matrix<double, NUM_STATE, NUM_POSE, Eigen::RowMajor>> jaco(jacobian);
-    jaco.setZero();
+    jaco.setZero();  // 初始化为零矩阵
 
+    // 填充雅克比矩阵的块
     jaco.block(0, 0, 3, 3) = state0.q.inverse().toRotationMatrix();
     jaco.block(6, 3, 3, 3) =
         Rotation::quaternionleft(corrected_q_.inverse() * state0.q.inverse() * state1.q).bottomRightCorner<3, 3>();
 
+    // 用平方根信息矩阵缩放雅克比矩阵
     jaco = sqrt_information_ * jaco;
     return jaco;
 }
@@ -107,12 +128,14 @@ Eigen::MatrixXd PreintegrationNormal::residualJacobianMix0(const IntegrationStat
     Eigen::Map<Eigen::Matrix<double, NUM_STATE, NUM_MIX, Eigen::RowMajor>> jaco(jacobian);
     jaco.setZero();
 
+    // 提取雅克比矩阵的各个块
     Eigen::Matrix3d dp_dbg = jacobian_.block<3, 3>(0, 9);
     Eigen::Matrix3d dp_dba = jacobian_.block<3, 3>(0, 12);
     Eigen::Matrix3d dv_dbg = jacobian_.block<3, 3>(3, 9);
     Eigen::Matrix3d dv_dba = jacobian_.block<3, 3>(3, 12);
     Eigen::Matrix3d dq_dbg = jacobian_.block<3, 3>(6, 9);
 
+    // 填充雅克比矩阵的块
     jaco.block(0, 0, 3, 3) = -state0.q.inverse().toRotationMatrix() * delta_time_;
     jaco.block(0, 3, 3, 3) = -dp_dbg;
     jaco.block(0, 6, 3, 3) = -dp_dba;
@@ -124,40 +147,44 @@ Eigen::MatrixXd PreintegrationNormal::residualJacobianMix0(const IntegrationStat
     jaco.block(9, 3, 3, 3)  = -Eigen::Matrix3d::Identity();
     jaco.block(12, 6, 3, 3) = -Eigen::Matrix3d::Identity();
 
+    // 用平方根信息矩阵缩放雅克比矩阵
     jaco = sqrt_information_ * jaco;
     return jaco;
 }
 
 Eigen::MatrixXd PreintegrationNormal::residualJacobianMix1(const IntegrationState &state0,
                                                            const IntegrationState &state1, double *jacobian) {
+    // 将 jacobian 指针映射到一个 Eigen 矩阵
     Eigen::Map<Eigen::Matrix<double, NUM_STATE, NUM_MIX, Eigen::RowMajor>> jaco(jacobian);
-    jaco.setZero();
+    jaco.setZero();  // 初始化为零矩阵
 
+    // 填充雅克比矩阵的块
     jaco.block(3, 0, 3, 3)  = state0.q.inverse().toRotationMatrix();
     jaco.block(9, 3, 3, 3)  = Eigen::Matrix3d::Identity();
     jaco.block(12, 6, 3, 3) = Eigen::Matrix3d::Identity();
 
+    // 用平方根信息矩阵缩放雅克比矩阵
     jaco = sqrt_information_ * jaco;
     return jaco;
 }
 
 int PreintegrationNormal::numResiduals() {
-    return NUM_STATE;
+    return NUM_STATE;  // 返回残差的数量
 }
 
 vector<int> PreintegrationNormal::numBlocksParameters() {
-    return std::vector<int>{NUM_POSE, NUM_MIX, NUM_POSE, NUM_MIX};
+    return std::vector<int>{NUM_POSE, NUM_MIX, NUM_POSE, NUM_MIX};  // 返回参数块的数量
 }
 
 IntegrationStateData PreintegrationNormal::stateToData(const IntegrationState &state) {
     IntegrationStateData data;
-    PreintegrationBase::stateToData(state, data);
+    PreintegrationBase::stateToData(state, data); // 调用基类的转换函数
     return data;
 }
 
 IntegrationState PreintegrationNormal::stateFromData(const IntegrationStateData &data) {
     IntegrationState state;
-    PreintegrationBase::stateFromData(data, state);
+    PreintegrationBase::stateFromData(data, state); // 调用基类的转换函数
     return state;
 }
 
@@ -180,7 +207,9 @@ void PreintegrationNormal::constructState(const double *const *parameters, Integ
     };
 }
 
+// 该函数执行预积分的主要过程，包括偏差补偿和状态更新。
 void PreintegrationNormal::integrationProcess(unsigned long index) {
+    //偏差补偿
     IMU imu_pre = compensationBias(imu_buffer_[index - 1]);
     IMU imu_cur = compensationBias(imu_buffer_[index]);
 
@@ -204,6 +233,7 @@ void PreintegrationNormal::updateJacobianAndCovariance(const IMU &imu_pre, const
 
     // jacobian
 
+    //构建雅克比矩阵的状态转移矩阵
     // phi = I + F * dt
     phi.block<3, 3>(0, 0)   = Matrix3d::Identity();
     phi.block<3, 3>(0, 3)   = Matrix3d::Identity() * dt;
@@ -215,23 +245,27 @@ void PreintegrationNormal::updateJacobianAndCovariance(const IMU &imu_pre, const
     phi.block<3, 3>(9, 9)   = Matrix3d::Identity() * (1 - dt / parameters_->corr_time);
     phi.block<3, 3>(12, 12) = Matrix3d::Identity() * (1 - dt / parameters_->corr_time);
 
+    //更新雅可比矩阵
     jacobian_ = phi * jacobian_;
 
     // covariance
 
     Eigen::MatrixXd gt = Eigen::MatrixXd::Zero(NUM_STATE, NUM_NOISE);
 
+    //构建噪声影响矩阵 gt 
     gt.block<3, 3>(3, 3)  = delta_state_.q.toRotationMatrix();
     gt.block<3, 3>(6, 0)  = Matrix3d::Identity();
     gt.block<3, 3>(9, 6)  = Matrix3d::Identity();
     gt.block<3, 3>(12, 9) = Matrix3d::Identity();
 
+    //计算新的协方差矩阵 Qk
     Eigen::MatrixXd Qk =
         0.5 * dt * (phi * gt * noise_ * gt.transpose() + gt * noise_ * gt.transpose() * phi.transpose());
+    //更新协方差矩阵 covariance_
     covariance_ = phi * covariance_ * phi.transpose() + Qk;
 }
 
-void PreintegrationNormal::resetState(const IntegrationState &state, int num) {
+void PreintegrationNormal::resetState(const IntegrationState &state, int num) {//重置积分状态
     delta_time_ = 0;
     delta_state_.p.setZero();
     delta_state_.q.setIdentity();
@@ -243,16 +277,17 @@ void PreintegrationNormal::resetState(const IntegrationState &state, int num) {
     covariance_.setZero(num, num);
 }
 
+//用于设置噪声矩阵 noise_，定义了系统中各个噪声项的大小。
 void PreintegrationNormal::setNoiseMatrix() {
     noise_.setIdentity(NUM_NOISE, NUM_NOISE);
-    noise_.block<3, 3>(0, 0) *= parameters_->gyr_arw * parameters_->gyr_arw; // nw
-    noise_.block<3, 3>(3, 3) *= parameters_->acc_vrw * parameters_->acc_vrw; // na
+    noise_.block<3, 3>(0, 0) *= parameters_->gyr_arw * parameters_->gyr_arw; // nw，设置陀螺仪角速率随机游走（gyr_arw）
+    noise_.block<3, 3>(3, 3) *= parameters_->acc_vrw * parameters_->acc_vrw; // na，设置加速度计速度随机游走（acc_vrw）
     noise_.block<3, 3>(6, 6) *=
-        2 * parameters_->gyr_bias_std * parameters_->gyr_bias_std / parameters_->corr_time; // nbg
+        2 * parameters_->gyr_bias_std * parameters_->gyr_bias_std / parameters_->corr_time; // nbg，设置陀螺仪偏置噪声（gyr_bias_std），并考虑到时间相关性
     noise_.block<3, 3>(9, 9) *=
-        2 * parameters_->acc_bias_std * parameters_->acc_bias_std / parameters_->corr_time; // nba
+        2 * parameters_->acc_bias_std * parameters_->acc_bias_std / parameters_->corr_time; // nba，设置加速度计偏置噪声（acc_bias_std），并考虑到时间相关性
 }
 
-int PreintegrationNormal::numMixParametersBlocks() {
+int PreintegrationNormal::numMixParametersBlocks() {// 该函数返回混合参数块的数量。
     return NUM_MIX;
 }
